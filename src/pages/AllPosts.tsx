664c3ee411fcd20003007cba9c8811c3d687e38e
Shiveen Pandita <shiveenpandita@fastmail.com>
2024-08-07 23:22:33 +1000

    Update post paths and reverse post list order
    Changed post paths in metadata.json by removing the "/public" prefix. Additionally, reversed the order of posts in AllPosts.tsx so the most recent entries appear first.

--- src/pages/AllPosts.tsx
+++ src/pages/AllPosts.tsx
@@ -8,7 +8,8 @@ const AllPosts = () => {
       <div className="flex flex-col items-center justify-center text-gray-600 body-font">
           <div className="pl-6 pt-10">
               <ul className="list-disc list-inside pt-30">
-                  {posts.map((post, index) => (
+                  {/* We reverse here since the json file has the most recent entry at the bottom */}
+                  {posts.reverse().map((post, index) => (
                       <li key={index} className="p-1">
                           <Link to={`/post?path=${encodeURIComponent(post.path)}`}>
                         <span className="font-semibold text-gray-600 hover:underline">
