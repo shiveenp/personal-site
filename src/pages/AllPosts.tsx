3992946357cb316f8d43320f55d2b852aa36fde1
Shiveen Pandita <shiveenpandita@fastmail.com>
2024-08-09 17:15:32 +1000

    Added dates to posts

--- src/pages/AllPosts.tsx
+++ src/pages/AllPosts.tsx
@@ -8,13 +8,14 @@ const AllPosts = () => {
             <div className="pl-6 pt-10">
                 <ul className="list-disc list-inside pt-30">
                     {/* We reverse here since the json file has the most recent entry at the bottom */}
-                    {posts.reverse().map((post) => (
+                    {posts.map((post) => (
                         <li className="p-1">
                             <Link to={`/post?path=${encodeURIComponent(post.path)}`}>
                         <span className="font-semibold text-gray-600 hover:underline">
                             {post.title}
                         </span>
                             </Link>
+                            <span className="font-light text-sm pl-2">{post.date}</span>
                         </li>
                     ))}
                 </ul>
--- src/pages/HomePagePosts.tsx
+++ src/pages/HomePagePosts.tsx
@@ -4,13 +4,14 @@ import {Link} from "react-router-dom";
 const HomePagePosts = () => {
     return (
         <ul className="list-disc list-inside">
-            {posts.slice(-5).map((post, index) => (
+            {posts.slice(0, 5).map((post, index) => (
                 <li key={index} className="p-1">
                     <Link to={`/post?path=${encodeURIComponent(post.path)}`}>
                         <span className="font-semibold text-gray-800 hover:underline">
                             {post.title}
                         </span>
                     </Link>
+                    <span className="font-light text-sm pl-2">{post.date}</span>
                 </li>
             ))}
         </ul>
